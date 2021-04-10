4ad42c9ee0922ae1ffc7f61a6a08484adc4de1db
Daurbek Sakhtarov <61458120+daurbekSD@users.noreply.github.com>
2021-04-10 19:05:49 +0500

    adapter realized

--- authorization/app/src/main/java/com/example/authorization/recyclerviews/RecyclerViewActivity.kt
+++ authorization/app/src/main/java/com/example/authorization/recyclerviews/RecyclerViewActivity.kt
@@ -5,14 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.authorization.R
+import com.example.authorization.recyclerviews.models.Pizza
+import com.example.authorization.recyclerviews.recycler_view_components.PizzaRVAdapter
 
 class RecyclerViewActivity: AppCompatActivity() {
+    private val pizzas= listOf<Pizza>(
+        Pizza("Pomidor",2000,""),
+        Pizza("Ananas",3000,""),
+        Pizza("Vegetables",2300,""),
+        Pizza("Gribi",1400,""),
+        Pizza("Maionez",3000,""),
+        Pizza("Peperoni",1550,""),
+        Pizza("Pomidor2",1900,"")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
 
         val recyclerViewPizza=findViewById<RecyclerView>(R.id.rv)
         recyclerViewPizza.layoutManager=LinearLayoutManager(this)
-
+        recyclerViewPizza.adapter=PizzaRVAdapter(pizzas)
     }
 }
