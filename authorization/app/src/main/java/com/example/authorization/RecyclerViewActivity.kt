650bb7422e39786ef036171b89f06d3183d1c168
Daurbek Sakhtarov <61458120+daurbekSD@users.noreply.github.com>
2021-04-10 18:40:50 +0500

    rv

--- authorization/app/src/main/java/com/example/authorization/RecyclerViewActivity.kt
+++ authorization/app/src/main/java/com/example/authorization/RecyclerViewActivity.kt
@@ -2,10 +2,16 @@ package com.example.authorization
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
+
+        val recyclerViewPizza=findViewById<RecyclerView>(R.id.rv)
+        recyclerViewPizza.layoutManager=LinearLayoutManager(this)
+
     }
 }
