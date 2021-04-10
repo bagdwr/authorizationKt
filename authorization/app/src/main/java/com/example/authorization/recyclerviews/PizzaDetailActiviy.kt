f1a00622e223975188875a1a3368f8e2e652fff2
Daurbek Sakhtarov <61458120+daurbekSD@users.noreply.github.com>
2021-04-10 19:57:08 +0500

    PizzaDetailActiviy.kt

---
+++ authorization/app/src/main/java/com/example/authorization/recyclerviews/PizzaDetailActiviy.kt
@@ -0,0 +1,16 @@
+package com.example.authorization.recyclerviews
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.authorization.R
+
+class PizzaDetailActiviy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_pizza_detail)
+
+
+    }
+
+}
